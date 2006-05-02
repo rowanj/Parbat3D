542890ad69b7fc753296e39b63024d5df25d0694
Rowan James <rowanj@burninator.net>
2006-05-02 14:09:53 +0000

    Added - includes defines for debugging and testing

---
+++ config.h
@@ -0,0 +1,23 @@
+// Guard against multiple inclusion
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+// Enable global debug/trace mode
+#define DEBUG 1
+//#undef DEBUG
+
+// Declare some packages as optional during initial implementation.
+//  This is to allow other packages to build for testing.
+#undef TMP_USE_IMAGE_FILE
+#define TMP_USE_IMAGE_MANIPULATION 1
+#undef TMP_USE_SETTINGS
+
+// Platform-specific portions of code should test for the relevant PLATFORM_ define
+#ifdef WIN32
+#define PLATFORM_W32 1
+#else
+// 32-bit Windows is currently the only platform we support.
+#error Unsupported platform detected.  See config.h
+#endif
+
+#endif
