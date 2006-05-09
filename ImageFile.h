64c2bddf49b35658ae794b6b910c3f3b0785b59f
Dafydd Williams <pipeline_optika@users.sourceforge.net>
2006-05-09 12:06:12 +0000

    Addded the GDAL header to appropriate files.

--- ImageFile.h
+++ ImageFile.h
@@ -1,5 +1,7 @@
 #ifndef _IMAGE_FILE_H
 #define _IMAGE_FILE_H
 
+#include <gdal_priv.h>
+
 #endif
 
--- ImageProperties.h
+++ ImageProperties.h
@@ -1,5 +1,7 @@
 #ifndef _IMAGE_PROPERTIES_H
 #define _IMAGE_PROPERTIES_H
 
+#include <gdal_priv.h>
+
 #endif
 
