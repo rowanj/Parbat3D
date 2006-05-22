38c0c06495ee549a1b1754cd33bad829c5cf8285
Rowan James <rowanj@burninator.net>
2006-05-22 14:18:11 +0000

    Re-arranged band numbers in ToolWindowDisplayTabContainerProcedure to test data re-ordering in ImageTileSet.

--- main.cpp
+++ main.cpp
@@ -908,7 +908,8 @@ LRESULT CALLBACK ToolWindowDisplayTabContainerProcedure(HWND hwnd, UINT message,
                         (LPSTR) szMainWindowClassName,
             			MB_ICONINFORMATION | MB_OK );
             // !! Insert band numbers (bands start at 1, not 0) here. - Rowan
-            if (image_handler) image_handler->set_bands(1,2,3);
+            if (image_handler) image_handler->set_bands(3,1,2);
+            // !! Force redraw
             }                
             break;
         
