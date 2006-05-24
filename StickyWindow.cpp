5807a672854d32a81cabe49c77dee0cf4d885af6
Shane Kingston <shanek1024@users.sourceforge.net>
2006-05-24 01:35:50 +0000

    minor changes to stickywindow, changed file menu item labels

--- StickyWindow.cpp
+++ StickyWindow.cpp
@@ -1,57 +1,5 @@
 //note: do not use these classes yet
-
-#include <vector>
-#include <set>
-#include <algorithm>
-#include <Windows.h>
-
-using std::set;
-using std::vector;
-
-class MovingStickyWindow;
-
-class StickyWindow
-{
-    protected:
-        HWND  hwnd;
-        set<StickyWindow*> stickTo;
-        set<StickyWindow*> attract;
-        vector<MovingStickyWindow*> moving;
-        set<StickyWindow*> snapped;
-        POINT mouseOffset;     /* mouse offset relative to window, used for snapping */
-        RECT startMovePosition;
-        //POINT sizeMousePosition;   /* mouse position, used for sizing window */
-        //RECT  sizeWindowPosition;   /* window position, used for sizing window */
-    public:
-        StickyWindow(HWND hwin);
-        void stickToWindow(StickyWindow *stickyWin);
-        void attractWindow(StickyWindow *stickyWin);    
-        void stopStickingTo(StickyWindow *stickyWin);
-        void stopAttracting(StickyWindow *stickyWin);
-        void removeStickyWindow(StickyWindow *stickyWin);
-        void onStartMove(int x,int y);
-        void onMoving(RECT* rect);
-        void onSize();    
-        void onStartSize();
-        void onSizing(); 
-        HWND getHWND() {return hwnd;};  
-        void setNewWindowPosition(RECT* newPos);     
-        int snapWindowByMoving(HWND snapToWin,RECT *rect);
-        void getMouseWindowOffset(int mx,int my);
-        //void moveBy(int x,int y);
-        SIZE* getDistanceMoved(RECT *rect);
-};
-
-class MovingStickyWindow
-{
-    protected:
-        RECT *originalRect;
-        StickyWindow *win;
-    public:
-        MovingStickyWindow(StickyWindow* sticky,RECT* rect) {win=sticky;originalRect=rect;};
-        void moveBy(int,int);
-        StickyWindow* getStickyWindow() {return win;};
-};
+#include "StickyWindow.h"
 
 StickyWindow::StickyWindow(HWND win)
 {
@@ -302,13 +250,7 @@ SIZE* StickyWindow::getDistanceMoved(RECT *rect)
 
 void MovingStickyWindow::moveBy(int x,int y)
 {
-    RECT rect;
     HWND hwnd=this->win->getHWND();
-    GetWindowRect(hwnd,&rect);
-    rect.left+=x;
-    rect.top+=y;
-    rect.right+=x;
-    rect.bottom+=y;
-    MoveWindow(hwnd,rect.left,rect.top,rect.right,rect.bottom,true);
-    //note: need to move relative to original position
+    MoveWindow(hwnd,originalRect->left+x,originalRect->top+y,
+                        originalRect->right+x,originalRect->bottom+y,true);
 }
