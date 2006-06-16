95527b91808dca1fa39ed6603c6a2c88ae912aee
Rowan James <rowanj@burninator.net>
2006-06-16 08:36:16 +0000

    Repository re-structuring

--- TestUtils.cpp
+++
@@ -1,9 +0,0 @@
-#include "TestUtils.h"
-
-void attemptRasterCall(ImageFile* theFile)
-{
-    char* theBuffer=new char[256*256*3];
-    theFile->getRasterData(256, 256, 0, 0,
-    						theBuffer, 256, 256);
-}
-
--- TestUtils.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TEST_UTILS_H
-#define _TEST_UTILS_H
-
-#include "ImageFile.h"
-
-void attemptRasterCall(ImageFile* theFile);
-
-#endif
