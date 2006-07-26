6e9819d06780063f4cb558047afe15d120fd3038
Shane Kingston <shanek1024@users.sourceforge.net>
2006-07-26 03:25:15 +0000

    Added comments for the class and each of the functions

--- trunk/IniFile.h
+++ trunk/IniFile.h
@@ -5,21 +5,65 @@
 
 using namespace std;
 
+/**
+    Handles access to ini files. Once a file name has been specified in the constructor,
+    you can parse (search) the file for values, read values or update values. The update
+    function can also be used to add values to the file.
+*/
 class iniFile {
-        bool gotFileName;
-        string fileName;
+        bool gotFileName;  // true if a filename has been specified
+        string fileName;   // stores the filename
         
+        /**
+            Returns the key part of a line in the file.
+        */
         string getKeyFromLine (string);
+        
+        /**
+            Returns the data (value) part of a line in the file.
+        */
         string getDataFromLine (string);
         
     public:
+        /**
+            Stores the filename to be used in future calls.
+            This was hopefully going to open the file once so it didn't have to be opened
+            again in each of the other calls but I could not get it to work. - Matt
+        */
         void open (string);
+        
+        /**
+            Deletes the filename so file calls cannot complete.
+            If open() worked correctly, this call would have closed the file as well as
+            deleting the filename. - Matt
+        */
         void close ();
 
-        bool parse (string);        
+
+        /**
+            Searches to find if the key provided as a parameter exists in the file. If the
+            key is in the file then true is returned, otherwise it is false.
+        */
+        bool parse (string);
+        
+        /**
+            Returns the value of the specified key in the file. If the key does not exist,
+            then an empty string is returned.
+            If open() worked correctly, if read occured after parse then the file would not
+            have to be searched again. - Matt
+        */
         string read (string);
+        
+        /**
+            Updates the key in the file with the specified value. If the key does not exist,
+            then it is added to the file with the value.
+        */
         void update (string, string);
         
+        
+        /**
+            Returns the name of the file that is being used.
+        */
         string getFileName () { return fileName; }
 };
 
--- trunk/Settings.h
+++ trunk/Settings.h
@@ -7,15 +7,41 @@
 
 using namespace std;
 
+/**
+    Allows settings to be saved to and retrieved from an ini file. Access to the file is
+    handled by IniFile.cpp and its functions.
+*/
 class settings {
-        iniFile openFile;
+        iniFile openFile;  // reference to the ini file, all file operations go through this
         
     public:
+        /**
+            Sets up access to the file using the filename given as a parameter.
+        */
         settings (string filename) { openFile.open(filename); }
-        ~settings () { openFile.close(); }
         
+        /**
+            Ends access to the file.
+        */
+        ~settings () { openFile.close(); }
+
+        
+        /**
+            Returns a string containing the value of the key. If the key does not exist in
+            the file then an empty string is returned.
+        */
         string getSetting (string);
+
+        /**
+            Adds the setting to the file.
+        */
         void setSetting (string, string);
+
+        /**
+            Adds the setting to the file. Since the file can only be updated using text,
+            the integer is first converted into a string using stringstream before updating
+            the file.
+        */
         void setSetting (string, int);
 };
 
