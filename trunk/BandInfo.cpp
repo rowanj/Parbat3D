dddbe547b0bff97d2d0d6c93ebadb03042e9d5bf
Dafydd Williams <pipeline_optika@users.sourceforge.net>
2006-07-30 05:15:23 +0000

    Checked code, and added comments to BandInfo.

--- trunk/BandInfo.cpp
+++ trunk/BandInfo.cpp
@@ -2,6 +2,27 @@
 
 #include "BandInfo.h"
 
+/*
+BandInfo
+
+========
+
+A BandInfo object is instantiated as part of the ImageFile class instantiation. It
+contains information about the spectral bands present in the image file. It calls
+GDAL where appropriate, and is passed a GDALRasterBand from which it obtains its
+data.
+
+Many of the GDAL calls do not produce any output for many file types. Things like
+Colour Interpretation Name, for example, do not seem to produce results.
+*/
+
+/*
+BandInfo::BandInfo
+
+Constructor for the BandInfo class. Stores the band number, the raster data type,
+the colour interpretation name (if available), the X and Y block sizes and the
+minimum and maximum values in this band.
+*/
 BandInfo::BandInfo(GDALRasterBand* theBand)
 {
 	#if DEBUG_BANDS
@@ -84,51 +105,101 @@ BandInfo::BandInfo(GDALRasterBand* theBand)
 	#endif //DEBUG_BANDS
 }
 
+/*
+BandInfo::getRasterDataType
+
+Returns the raster data type of the band.
+*/
 const char* BandInfo::getRasterDataType(void)
 {
 	return rasterDataType;
 }
 
+/*
+BandInfo::getColourInterpretationName
+
+Returns the colour interpretation name of the band.
+*/
 const char* BandInfo::getColourInterpretationName(void)
 {
 	return colourInterpretationName;
 }
 
+/*
+BandInfo::getDataMin
+
+Returns the minimum value of the band. Used for histograms and transforms.
+*/
 double BandInfo::getDataMin(void)
 {
 	return adfMinMax[0];
 }
 
+/*
+BandInfo::getDataMax
+
+Returns the maximum value of the band. Used for histograms and transforms.
+*/
 double BandInfo::getDataMax(void)
 {
 	return adfMinMax[1];
 }
 
+/*
+BandInfo::getBlockXSize
+
+Returns the block size of the band on the X axis.
+*/
 int BandInfo::getBlockXSize(void)
 {
 	return blockXSize;
 }
 
+/*
+BandInfo::getBlockYSize
+
+Returns the block size of the band on the Y axis.
+*/
 int BandInfo::getBlockYSize(void)
 {
 	return blockYSize;
 }
 
+/*
+BandInfo::getOverviewCount
+
+Returns the number of overviews for this band.
+*/
 int BandInfo::getOverviewCount(void)
 {
 	return overviewCount;
 }
 
+/*
+BandInfo::getBandNumber
+
+Returns the band number for this band.
+*/
 int BandInfo::getBandNumber(void)
 {
 	return bandNumber;
 }
 
+/*
+BandInfo::getBand
+
+Returns a pointer to the GDAL band structure itself.
+*/
 GDALRasterBand* BandInfo::getBand(void)
 {
 	return band;	
 }
 
+/*
+BandInfo::~BandInfo
+
+Deconstructor; currently does nothing.
+*/
 BandInfo::~BandInfo(void)
 {
 	return;
